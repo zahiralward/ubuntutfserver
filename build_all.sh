cb650e097b3e8f144a58b1c5df0f64f3822df563
Zahir Alward <zwfalward@hotmail.com>
2017-12-03 15:41:53 +1300

    Update build_all.sh
    Update Conda distribution to Python 3.x

--- build_all.sh
+++ build_all.sh
@@ -3,14 +3,15 @@ cd ~
 
 # You can change what anaconda version you want at 
 # https://repo.continuum.io/archive/
-wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh
-bash Anaconda2-5.0.1-Linux-x86_64.sh -b -p ~/anaconda
-rm Anaconda2-5.0.1-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p ~/anaconda
+rm Anaconda3-5.0.1-Linux-x86_64.sh
 echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc 
 
 # Refresh 
 source .bashrc
 conda update conda
+conda update --all
 
 # Git the TensorFlow latest respository
 git clone https://github.com/tensorflow/tensorflow /tmp/tensorgit
