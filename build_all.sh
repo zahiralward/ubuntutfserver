e3b5af491d03a49b982170ce5b5ae5492135330d
Zahir Alward <zwfalward@hotmail.com>
2017-12-03 21:34:50 +1300

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -16,3 +16,21 @@ conda update --all --yes
 # Git the TensorFlow latest respository
 git clone https://github.com/tensorflow/tensorflow /tmp/tensorgit
 cd /tmp/tensorgit/tensorflow
+
+# Install TensorFlow Python dependencies
+sudo apt-get install python3-numpy python3-dev python3-pip python3-wheel -y
+
+# Install using binary installer
+sudo apt-get install pkg-config zip g++ zlib1g-dev unzip -y
+wget https://github.com/bazelbuild/bazel/releases/download/0.8.0/bazel-0.8.0-installer-linux-x86_64.sh
+chmod +x bazel-0.8.0-installer-linux-x86_64.sh
+./bazel-0.8.0-installer-linux-x86_64.sh --user
+rm bazel-0.8.0-installer-linux-x86_64.sh
+echo 'export PATH="$PATH:$HOME/bin"' >> ~/.bashrc 
+# Refresh 
+source .bashrc
+
+# 4- Once installed, you can upgrade to a newer version of Bazel with:
+sudo apt-get upgrade bazel -y
+
+./configure
